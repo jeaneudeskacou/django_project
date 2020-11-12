72ebfce70a2b640afd7c5ab9f4fdb841e8da0e2a
jeaneudeskacou <kacoukbje@gmail.com>
2020-11-12 17:52:44 +0100

    correction of OneToOneField in Profil model and CharFields in Localisation

--- time_app/models.py
+++ time_app/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Localisation(models.Model):
-    continent = models.CharField()
-    city = models.CharField()
+    continent = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
     
     def __str__(self):
         return self.continent+"/"+self.city
 
 class Profil(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     localisations = models.ManyToManyField(Localisation)
 
     def __str__(self):
