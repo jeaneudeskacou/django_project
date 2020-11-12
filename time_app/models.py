15cc2d76eba65f1d99dd180cb752d6611625c822
jeaneudeskacou <kacoukbje@gmail.com>
2020-11-12 17:49:26 +0100

    add Localisation and Profil models

--- time_app/models.py
+++ time_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Localisation(models.Model):
+    continent = models.CharField()
+    city = models.CharField()
+    
+    def __str__(self):
+        return self.continent+"/"+self.city
+
+class Profil(models.Model):
+    user = models.OneToOneField(User)
+    localisations = models.ManyToManyField(Localisation)
+
+    def __str__(self):
+        return self.user.username
